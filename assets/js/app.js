6094c0db9411231adb053f2dc351b447dc53732a
Jonn Mostovoy <jm@serokell.io>
2020-01-18 03:07:53 +0200

    [RSCH-10] Demo PReact x Phoenix.js x Phoenix Channels
    Problem: there was no way for backend to communicate to frontend dynamically
    
    Solution:
    
    Use phoenix channels and embed the instantiation of the channel whitin the
    Preact component named ChannelDemo.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,31 @@
 import { h, Component, render } from 'preact'
+import socket from "./socket"
 
-const app = h('div', null, 'It works!')
+class ChannelDemo extends Component {
+  constructor() {
+    super()
+    this.state = { status: "Initializing" }
+  }
+  componentDidMount() {
+    let channel =
+    this.setState( { status: "Going",
+                     channel: socket.channel("location:C-100", {}) } )
+  }
+  onChannelOpen() {
+    this.state.channel.join()
+      .receive("ok", resp       => { this.setState( {status: "Connected"} ) })
+      .receive("error", reasons => { this.setState( {status: "Rejected with " + reasons} ) })
+      .receive("timeout", ()    => { this.setState( {status: "Networking issue"} ) })
+  }
+  componentWillUnmount() {
+    this.state.channel.leave()
+  }
+  render(props, state) {
+    if (state.status == 'Going') {
+      this.onChannelOpen()
+    }
+    return h('div', null, [ "Status of " + props.team, h('div', {style: 'color: red;'}, state.status) ])
+  }
+}
 
-render(app, document.getElementById('app_root'))
+render(h(ChannelDemo, {team: 'Team Test!'}), document.getElementById('app_root'))
--- lib/detect_web/channels/user_socket.ex
+++ lib/detect_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule DetectWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", DetectWeb.RoomChannel
+  channel "location:*", DetectWeb.LocationChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
